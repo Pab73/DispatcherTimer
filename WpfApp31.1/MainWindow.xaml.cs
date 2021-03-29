4f16539a7fa79ba636d634f5ccbc8f290b3c817a
PauloZM <Munoz.pablito@Gmail.com>
2021-03-29 13:27:33 +0200

    Initial commit

---
+++ WpfApp31.1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp31._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        private DateTime startTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Content = (DateTime.Now - startTime).ToString(@"mm\:ss");
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            timer.Start();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void btnReStart_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            startTime = DateTime.Now;
+            timer.Start();
+        }
+    }
+}
